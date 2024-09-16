e4ca1dedd808f74fdb9a1840ddf28d0fa0e9f587
Jz9015 <128329854+Jz9015@users.noreply.github.com>
2024-09-15 22:59:01 -0400

    Add files via upload

---
+++ primeFinder.py
@@ -0,0 +1,17 @@
+import time
+num = int(input("Enter a number: "))
+start = time.time()
+safety = float(num/2)
+testNum = 2
+output = float(1)
+while testNum<=safety:
+    output = num/testNum
+    testNum+=1
+    if output-round(output,0)==0:
+        print(num,"is not prime.","First factors are",testNum-1,f"{output:.0f}.")
+        end = time.time()
+        print(end-start)
+        exit()
+print(num,"is prime")
+end = time.time()
+print(end-start)
